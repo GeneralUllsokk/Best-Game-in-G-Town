6b83f97a3acd5c7711108a5d3b1ecd96f18ceb01
Emil Lyder Larsen <emilll16@uia.no>
2017-05-28 23:34:13 +0200

    Updated version number to 0.00.03

--- BestGame Desktop/core/src/com/bestgame/game/GameClass.java
+++ BestGame Desktop/core/src/com/bestgame/game/GameClass.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameClass extends Game {
-	private final String VERSION = "0.00.02";
+	private final String VERSION = "0.00.03";
 	public SpriteBatch batch;
 	public BitmapFont font;
 
